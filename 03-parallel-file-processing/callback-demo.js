df5bfc717bc6219270814f865a8cb09122bd1953
Allan Liebold <allan.liebold@gmail.com>
2017-09-16 16:31:24 -0700

    creates read and write demos

--- 03-parallel-file-processing/callback-demo.js
+++ 03-parallel-file-processing/callback-demo.js
@@ -1,12 +1,12 @@
 'use strict';
 
-// var someCallback = function(data) {
-//   console.log('got some data:', data);
-// }
+var someCallback = function(data) {
+  console.log('got some data:', data);
+}
 
-// var useCallback = function(cb) {
-//   cb(someCallback);
-// }
+var useCallback = function(cb) {
+  cb(someCallback);
+}
 
 var someCallback = function(err, data) {
   if (err) throw err;
@@ -18,3 +18,15 @@ var useCallback = function(cb) {
 }
 
 useCallback(someCallback);
+
+// var someCallback = function(err, data) {
+//   if (err) throw err;
+//   console.log('got some data:', data);
+// }
+//
+// var useCallback = function(cb) {
+//   var sampleError = new Error('throwing simulated error');
+//   cb(sampleError, 'no data');
+// }
+//
+// useCallback(someCallback);

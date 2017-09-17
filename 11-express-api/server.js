5bfe2dffa9bee0c1b9880a8529bf65479681a89d
Allan Liebold <allan.liebold@gmail.com>
2017-09-17 12:08:22 -0700

    creates server

--- 11-express-api/server.js
+++ 11-express-api/server.js
@@ -4,4 +4,10 @@ const express = require('express');
 const morgan = require('morgan');
 const createError = require('http-errors');
 const jsonParser = require('body-parser').json();
+
+const PORT = process.env.PORT || 3000;
 const app = express();
+
+app.listen(PORT, () => {
+  debug('server up:', PORT);
+});
